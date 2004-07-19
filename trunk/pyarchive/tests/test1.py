fc79624f69b26f6e6da580bc621f1f5ff59b57ac
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2004-07-19 03:24:41 +0000

    Fleshed out simple test case.

--- trunk/pyarchive/tests/test1.py
+++ trunk/pyarchive/tests/test1.py
@@ -1,4 +1,15 @@
-import pyarchive.ArchiveSubmission as pya
+import pyarchive as pya
+import pyarchive.submission as submission
 
-submission = pya.ArchiveItem('Statistikal', '', 'sound')
+submission = submission.ArchiveItem('AbleFatFingers', 
+                                        pya.const.OPENSOURCE_AUDIO, 
+                                        pya.const.AUDIO, 
+                                        'Fat Fingers')
+submission['runtime'] = '5:00'
+submission['license'] = 'http://creativecommons.org/licenses/by-sa/2.0/'
 
+file1 = submission.addFile('ff.mp3')
+file1.source = pya.const.ORIGINAL
+file1.format = "192Kbps MP3"
+
+print submission.submit('nathan@yergler.net', '3210chs')
