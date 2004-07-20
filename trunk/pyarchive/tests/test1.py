5d5cbf50c6913af0ea6b68b443971dcd17511f5b
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2004-07-20 17:07:18 +0000

    Modifications for new pyarchive.submission API.

--- trunk/pyarchive/tests/test1.py
+++ trunk/pyarchive/tests/test1.py
@@ -1,15 +1,15 @@
 import pyarchive as pya
 import pyarchive.submission as submission
 
-submission = submission.ArchiveItem('AbleFatFingers', 
+submission = submission.ArchiveItem('barefootbrothers-genocide', 
                                         pya.const.OPENSOURCE_AUDIO, 
                                         pya.const.AUDIO, 
-                                        'Fat Fingers')
-submission['runtime'] = '5:00'
-submission['license'] = 'http://creativecommons.org/licenses/by-sa/2.0/'
+                                        'Genocide')
+# submission['runtime'] = '5:20'
+submission['license'] = 'http://creativecommons.org/licenses/by-sa/1.0/'
 
-file1 = submission.addFile('ff.mp3')
-file1.source = pya.const.ORIGINAL
-file1.format = "192Kbps MP3"
+file1 = submission.addFile('GenocideHI.mp3',
+                           pya.const.ORIGINAL,
+                           pya.const.MP3_128K)
 
 print submission.submit('nathan@yergler.net', '3210chs')
