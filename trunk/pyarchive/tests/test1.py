b612773808e49536b969500e3a7aca001dd667aa
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2004-08-05 19:28:34 +0000

    Testing with non-moderator account.

--- trunk/pyarchive/tests/test1.py
+++ trunk/pyarchive/tests/test1.py
@@ -1,15 +1,17 @@
 import pyarchive as pya
 import pyarchive.submission as submission
 
-submission = submission.ArchiveItem('barefootbrothers-genocide', 
+submission = submission.ArchiveItem('sdfsdfaa-naughty', 
                                         pya.const.OPENSOURCE_AUDIO, 
                                         pya.const.AUDIO, 
-                                        'Genocide')
+                                        'Naughty')
 # submission['runtime'] = '5:20'
-submission['license'] = 'http://creativecommons.org/licenses/by-sa/1.0/'
+submission['license'] = 'http://creativecommons.org/licenses/by-sa/2.0/'
+submission[pya.const.CREATOR] = '_aa_'
+submission[pya.const.NOTES] = 'More information available at http://www.anal0g.org/'
 
-file1 = submission.addFile('GenocideHI.mp3',
+file1 = submission.addFile('sdflkknaughty.mp3',
                            pya.const.ORIGINAL,
                            pya.const.MP3_128K)
 
-print submission.submit('nathan@yergler.net', '3210chs')
+print submission.submit('archive@yergler.net', 'xxx')
