7e481bfc79e1a41e262f8b07082cd9fe14f907de
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2005-01-05 21:53:00 +0000

    Merge back changes from 1.0 branch.

--- trunk/cctagutils/metadata.py
+++ trunk/cctagutils/metadata.py
@@ -8,13 +8,10 @@ __version__ = "$Revision$"
 __copyright__ = '(c) 2004, Creative Commons, Nathan R. Yergler'
 __license__ = 'licensed under the GNU GPL2'
 
-# use the bundled version of PyTagger,
-# which contains our fixes.
-
-### TODO: Merge back changes from metadata on 1.0 branch
-
-#import tagger
+# use the bundled version of PyTagger, which contains our fixes.
+import tagger
 import eyeD3
+import os
 import cctagutils.const as const
 
 class AudioMetadata:
@@ -39,18 +36,37 @@ class AudioMetadata:
     def embed(self, license, verification, year, holder):
         """Embed a license claim in the audio file."""
         raise NotImplementedError()
+
+    def isWritable(self):
+        """Returns true if the user has permission to change the metadata."""
+        raise NotImplementedError()
     
 class Mp3Metadata(AudioMetadata):
     def __init__(self, filename):
         AudioMetadata.__init__(self, filename)
+        self.__open(filename)
+
+    def __open(self, filename=None):
+        if filename is not None:
+            self.filename = str(filename)
 
         # create a handle for ID3v2
-	self.__tag = eyeD3.Tag()
-	self.__tag.link(str(filename))
+        self.__tag = eyeD3.Tag()
+        try:
+            self.__tag.link(self.filename)
+        except eyeD3.tag.TagException, e:
+            if "2.2" in e.msg:
+                print 'aieee!'
+                self.__tag = None
+            else:
+                raise
 
     def _getFrame(self, fids):
         """Returns the first frame whose ID is contained in the tuple fids.
         Returns None if the frame identifiers do not exist."""
+
+        if self.__tag is None:
+            return None
 
         for frame in self.__tag.frames:
             if frame.header.id in fids:
@@ -64,18 +80,18 @@ class Mp3Metadata(AudioMetadata):
         frame = self._getFrame(fids)
 
         if frame is not None:
-	   if isinstance(frame, eyeD3.frames.DateFrame):
-	      return frame.getYear()
-	   else:
-	      return frame.data
+           if isinstance(frame, eyeD3.frames.DateFrame):
+              return frame.getYear()
+           else:
+              return frame.data
         
         return None
     
     def getTitle(self):
-        return self.__tag.getTitle();
+        return (self.__tag and self.__tag.getTitle()) or "";
 
     def getArtist(self):
-	return self.__tag.getArtist();
+        return (self.__tag and self.__tag.getArtist()) or "";
 
     def getYear(self):
         return self._getFrameData(('TYE', 'TYER', 'TDRC')) or ''
@@ -85,21 +101,50 @@ class Mp3Metadata(AudioMetadata):
     
     def _needsUpgrade(self):
         """Returns True if a file has ID3 tags of v2.2."""
-	print self.__tag.frames[0].header.majorVersion, self.__tag.frames[0].header.minorVersion
-
-	if self.__tag and len(self.__tag.frames) > 0:
-	   return ((self.__tag.frames[0].header.majorVersion >= 2) and 
+        if self.__tag is None:
+            return True
+        elif (self.__tag and len(self.__tag.frames) > 0):
+           return ((self.__tag.frames[0].header.majorVersion >= 2) and 
                    (self.__tag.frames[0].header.minorVersion >= 3))
         else:
-	   # either no ID3 information or no frames; 
-	   # in either case, no upgrade is neccessary
+           # either no ID3 information or no frames; 
+           # in either case, no upgrade is neccessary
            return False
 
     def upgrade(self):
         """Upgrades a file's ID3 tags from ID3v2.2 to ID3v2.3."""
 
-	self.__tag.update(eyeD3.ID3_V2_3)
+        # open the file using tagger
+        self.__v2 = tagger.id3v2.ID3v2(self.filename,
+                                       tagger.constants.ID3_FILE_MODIFY)
 
+        # retrieve the existing frames
+        oldframes = {}
+        for frame in self.__v2.frames:
+            oldframes[frame.fid] = (frame.rawdata, frame.length)
+            
+        # re-open the file for writing
+        self.__v2 = tagger.id3v2.ID3v2(self.filename,
+                                       mode=tagger.constants.ID3_FILE_NEW,
+                                       version=2.3)
+        
+        # rewrite each frame
+        for fid in oldframes:
+            if fid not in const.TAG_MAP or const.TAG_MAP[fid] is None:
+                # no mapping for this tag
+                print "Field can not be converted from 2.2 to 2.3: ", fid
+                continue
+
+            newframe = self.__v2.new_frame(fid=const.TAG_MAP[fid])
+            newframe.rawdata, newframe.length = oldframes[fid]
+            
+            newframe.parse_field()
+
+            self.__v2.frames.append(newframe)
+
+        self.__v2.commit()
+
+    
     def _addId3v1(self):
         """Checks for the existance of ID3v1 data in the specified file;
         if it does not exist, generates data from the ID3v2 tags.
@@ -122,25 +167,30 @@ class Mp3Metadata(AudioMetadata):
         self.__hasV1 = True
 
     def __clearTcop(self):
-	for f in self.__tag.frames:
-	    if f.header.id == 'TCOP':
-	       del f
+        for f in self.__tag.frames:
+            if f.header.id == 'TCOP':
+               del f
 
     def setClaim(self, claim):
 
         # check if an upgrade to 2.3 is needed before embedding
         if (self._needsUpgrade()):
+            # update tags to ID3v2.3
             self.upgrade()
 
+        # reopen the file (in case of 2.2)
+        self.__open()
+        print self.__tag
+
         # set the TCOP frame
-	self.__clearTcop()
-	header = eyeD3.frames.FrameHeader()
-	header.id = 'TCOP'
-	header.compressed = 0
+        self.__clearTcop()
+        header = eyeD3.frames.FrameHeader()
+        header.id = 'TCOP'
+        header.compressed = 0
         tcop = eyeD3.frames.TextFrame(header, text=unicode(claim))
 
-	self.__tag.frames.append(tcop)
-	self.__tag.update()
+        self.__tag.frames.append(tcop)
+        self.__tag.update()
 
     def embed(self, license, verification, year, holder):
         
@@ -153,6 +203,9 @@ class Mp3Metadata(AudioMetadata):
         # add ID3v1 if necessary
         # self._addId3v1()
 
+    def isWritable(self):
+        """Returns true if the user has permission to change the metadata."""
+        return os.access(self.filename, os.W_OK)
     
 class OggMetadata(AudioMetadata):
     pass
