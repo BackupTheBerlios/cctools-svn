01e81d675a4a260e08975371cf04f45f3ec47bf4
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2005-04-29 20:43:24 +0000

    Initial implementation of ZCML directives for metadata and wizard page declarations.

---
+++ trunk/publisher/p6/configure/metaconfigure.py
@@ -0,0 +1,83 @@
+import p6
+import p6.ui.pages
+from p6.metadata.base import metadatafield, metadatagroup
+
+class MGroupDirective(object):
+    """A metadata group."""
+
+    def __init__(self, _context, id, title, for_):
+
+        self.fields = []
+        self.appliesTo = for_
+        self.id = id
+        self.title = title
+
+        print 'adding mgroup action...'
+        _context.action(discriminator=('MetadataGroup', self.id),
+                        callable=self.__addGroup,
+                        args=[],
+                        order=-10
+                        )
+
+    def field(self, _context, id, type, label='', choices=[]):
+        if label == '':
+            label = id
+
+        self.fields.append(
+            metadatafield(type)(id, label, choices=choices)
+            )
+
+    def __addGroup(self):
+        """Add the group to the groups stack once configuration is complete."""
+        print self.id
+        p6.getApp().groups.append(
+            metadatagroup(self.appliesTo)(self.id, self.title,
+                                          self.fields)
+            )
+        
+class PagesDirective(object):
+    """Definition of a set of pages."""
+
+    def __init__(self, _context, appid):
+        # register the App ID
+        p6.getApp().appid = appid
+
+        # initialize the page registry
+        p6.getApp().pages = []
+        
+    def page(self, _context, type_, title=''):
+        pass
+
+    def metadatapages(self, _context, for_):
+        _context.action(discriminator=('GeneratePages',),
+                        callable=self.__generatePages,
+                        args=(for_,),
+                        )
+
+    def __generatePages(self, for_):
+        for page in p6.ui.pages.metadata.generatePages(for_):
+            p6.getApp().pages.append(page)
+            
+    def storepage(self, _context):
+        _context.action(discriminator=('RegisterPage', 'StorePage',
+                                       p6.ui.pages.StorePage),
+                        callable=p6.getApp().pages.append,
+                        args=(p6.ui.pages.StorePage,),
+                        )
+
+    def xmlpage(self, _context):
+        _context.action(discriminator=('RegisterPage', 'XmlDisplayPage',
+                                       p6.ui.pages.XmlMetadataPage),
+                        callable=p6.getApp().pages.append,
+                        args=(p6.ui.pages.XmlMetadataPage,),
+                        )
+
+
+    def xrcpage(self, _context, title, xrcfile, xrcid):
+        _context.action(discriminator=('RegisterPage', xrcid,
+                                       p6.ui.pages.XrcPage),
+                        callable=p6.getApp().pages.append,
+                        args=(p6.ui.pages.xrcpage(title, xrcfile, xrcid),),
+                        )
+
+    
---
+++ trunk/publisher/p6/configure/metadirectives.py
@@ -0,0 +1,121 @@
+"""Component architecture related 'zope' ZCML namespace directive interfaces
+
+$Id$
+"""
+__docformat__ = 'restructuredtext'
+
+import zope.configuration.fields
+import zope.interface
+import zope.schema
+
+_ = unicode
+
+class IMdataGroupDirective(zope.interface.Interface):
+    """Metadata grouping."""
+
+    id = zope.configuration.fields.MessageID(
+        title=u"Unique identifier",
+        description=u"",
+        required=True,
+        )
+
+    title= zope.configuration.fields.MessageID(
+        title=u"Unique identifier",
+        description=u"",
+        required=True,
+        )
+
+    for_ = zope.configuration.fields.GlobalInterface(
+        title=u"",
+        description=u"",
+        required=True,
+        )
+
+class IMetadataFieldSubdirective(zope.interface.Interface):
+    """  """
+
+    id = zope.configuration.fields.MessageID(
+        title=u"",
+        description=u"",
+        required=True,
+        )
+
+    type = zope.configuration.fields.GlobalInterface(
+        title=u"",
+        description=u"",
+        required=True,
+        )
+
+    label = zope.configuration.fields.MessageID(
+        title=u"",
+        description=u"",
+        required=False,
+        )
+
+    choices = zope.configuration.fields.Tokens(
+        title=u"",
+        description=u"",
+        required=False,
+        value_type=zope.configuration.fields.MessageID(),
+        )
+    
+class IPagesDirective(zope.interface.Interface):
+    """Assembly of pages which defines a wizard-like application."""
+
+    appid = zope.configuration.fields.Tokens(
+        title=u"The application ID.",
+        required=True,
+        value_type=zope.configuration.fields.PythonIdentifier(),
+        )
+
+class IPageSubdirective(zope.interface.Interface):
+    """A single page."""
+
+    factory = zope.configuration.fields.GlobalObject (
+        title=u"The page factory.",
+        description=u"The page factory; the factory should take a single "
+               "argument,"
+               " the parent window, and return an object conforming to "
+               "IWizardPage.",
+        required=True,
+
+        )
+
+class IMetadataPagesSubdirective(zope.interface.Interface):
+    """A sequence of pages generated from the metadata."""
+
+    for_ = zope.configuration.fields.Tokens(
+        title=_("Interfaces"),
+        description=_("The interfaces which metadata applies to."),
+        required=False,
+        value_type=zope.configuration.fields.GlobalInterface(),
+        )
+
+class IStorePageSubdirective(zope.interface.Interface):
+    """A generic storage page which is able to emit and handle the
+    appropriate events."""
+
+class IXmldisplayPageSubdirective(zope.interface.Interface):
+    """A generic page which displays a psuedo-XML representation of the
+    metadata collected."""
+    
+class IXrcPageSubdirective(zope.interface.Interface):
+    """A customizable XRC page."""
+
+    title = zope.configuration.fields.MessageID(
+        title=u"The page title.",
+        required=True,
+        )
+
+    xrcfile = zope.configuration.fields.Path(
+        title=u"XRC filename",
+        description=u"",
+        required=True,
+        )
+    
+    xrcid = zope.configuration.fields.MessageID(
+        title=u"XRC Id",
+        description=u"",
+        required=True,
+        )
+
