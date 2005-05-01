dd4363f7c66b929f8f25329ec30ca30ad6328fef
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2005-05-01 01:23:13 +0000

    Added event listener for item modification events to root-level app object.

--- trunk/publisher/p6/app/wxpy.py
+++ trunk/publisher/p6/app/wxpy.py
@@ -1,6 +1,7 @@
 import wx
 
 import zope.interface
+import zope.component
 
 import p6
 import interfaces
@@ -36,4 +37,21 @@ class WizApp(wx.App):
 
         self.SetTopWindow(self.main)
 
+        self.connectEvents()
+        
         return True
+
+    def connectEvents(self):
+
+        # listen for item addition events
+        zope.component.provideHandler(
+            zope.component.adapter(p6.storage.events.IItemSelected)(
+                p6.api.deinstify(self.selectItem))
+            )
+
+        # create the root item
+        self.items.append(p6.storage.items.RootItem())
+        
+    def selectItem(self, event):
+        self.items.append(event.item)
+
