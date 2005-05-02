da4155cb5b3ad7a3a68e49a88c12dbe7d30eb214
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2005-05-02 16:53:53 +0000

    Added findField convenience method.

--- trunk/publisher/p6/api.py
+++ trunk/publisher/p6/api.py
@@ -1,5 +1,6 @@
 import wx
 import zope.interface
+import p6
 
 def deinstify(func):
     def foo(*args, **kwargs):
@@ -18,3 +19,35 @@ def nearest(items, target):
 
 def getApp():
     return wx.GetApp()
+
+
+def findField(field_id, item=None):
+    if item is None:
+        print
+        print 'event.item is None'
+        print 'event.key is ', field_id
+        
+        # no item discriminator; just find the first default mention of this
+        # field name
+        for g in p6.api.getApp().groups:
+            for f in g.getFields():
+                print 'checking ', f.id
+                if f.id == field_id:
+                    return f()
+    else:
+        # find the metadata for this type of item and work from there
+        for g in p6.api.getApp().groups:
+            meta_dicts = [n[1] for n in
+                         zope.component.getGlobalSiteManager().getAdapters(
+               (g, item),
+               p6.metadata.interfaces.IMetadataDict)
+                         if n]
+
+            for m in meta_dicts:
+                if field_id in m:
+                    return m[field_id]
+
+    return None
+
+    print event
+    print event.key, event.item
