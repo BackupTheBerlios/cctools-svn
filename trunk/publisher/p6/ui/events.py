458052af054c5439440a2d1a29d4ed6ad4eb55fb
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2005-05-10 18:29:45 +0000

    Added event for resetting status bar.

--- trunk/publisher/p6/ui/events.py
+++ trunk/publisher/p6/ui/events.py
@@ -5,6 +5,10 @@ class IUpdateStatus(zope.interface.Interface):
     value = zope.interface.Attribute("")
     delta = zope.interface.Attribute("")
 
+class IResetStatus(zope.interface.Interface):
+    message = zope.interface.Attribute("")
+    steps = zope.interface.Attribute("")
+    
 class UpdateStatusEvent:
     zope.interface.implements(IUpdateStatus)
 
@@ -12,3 +16,11 @@ class UpdateStatusEvent:
         self.message = message
         self.value = value
         self.delta = delta
+
+class ResetStatusEvent:
+    zope.interface.implements(IResetStatus)
+
+    def __init__(self, message='', steps=0):
+        self.message = message
+        self.steps = steps
+        
