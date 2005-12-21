57d155551ba3597fce3a0775eb9a8c604386c343
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2005-12-21 20:13:28 +0000

    Include ZCML files in py2exe distributions.

--- publisher/trunk/appsetup.py
+++ publisher/trunk/appsetup.py
@@ -11,6 +11,7 @@ $Id$
 from distutils.core import setup, Extension
 import os
 import platform
+import fnmatch
 
 PLATFORM = platform.system().lower()
 
@@ -19,6 +20,19 @@ if PLATFORM == 'windows':
 elif PLATFORM == 'darwin':
     import py2app
 
+def findZcml():
+    result = []
+    
+    basePath = os.getcwd()
+    
+    for (dirpath, dirnames, filenames) in os.walk(basePath):
+        relpath = dirpath[len(basePath)+1:]
+        zcml = fnmatch.filter(filenames, "*.zcml")
+        
+        if len(zcml) > 0:
+            result.append((relpath, [os.path.join(relpath, n) for n in zcml]))
+
+    return result
 
 extensions = [Extension('zope.proxy._zope_proxy_proxy',
                         ['zope/proxy/_zope_proxy_proxy.c'],
@@ -26,52 +40,63 @@ extensions = [Extension('zope.proxy._zope_proxy_proxy',
                         )
               ]
 
-setup(name='ccPublisher',
-      version='1.9',
-      description = "Desktop tools for licensing works and uploading to the "
-                    "Internet Archive for hosting and cataloging.",
-      long_description="",
-      url='http://creativecommons.org',
-      author='Nathan R. Yergler',
-      author_email='nathan@creativecommons.org',
-      classifiers= ['Development Status :: 5 - Production/Stable',
-                    'Environment :: MacOS X :: Cocoa',
-                    'Environment :: Win32 (MS Windows)',
-                    'Environment :: X11 Applications :: GTK',
-                    'Intended Audience :: End Users/Desktop',
-                    'License :: OSI Approved :: GNU General Public License (GPL)',
-                    'Natural Language :: English',
-                    'Operating System :: OS Independent',
-                    'Programming Language :: Python',
-                    'Topic :: Multimedia',
-                    'Topic :: System :: Archiving',
-    ],
-      py_modules=['appsetup','setup' ],
-      scripts=['main.py'],
-      windows=['main.py'],
-      ext_modules=extensions,
-      packages=['ccpublisher',
-                'p6', 'p6.app', 'p6.configure',
-                'p6.metadata', 
-                'p6.storage','p6.storage.providers',
-                'p6.ui', 'p6.ui.pages', 'p6.ui.windows',
-                'p6.zcmlsupport',
-                'pyarchive', 'ccrdf', 'cctagutils', 'tagger', 'ccwsclient',
-                'ccwx','eyeD3',
-                'rdflib',
-                'rdflib.backends',
-                'rdflib.sparql',
-                'rdflib.syntax',
-                'rdflib.syntax.serializers',
-                'rdflib.syntax.parsers',
-                'zope',
-                'zope.component', 'zope.component.bbb',
-                'zope.component.bbb.tests',
-                'zope.configuration', 'zope.deprecation',
-                'zope.event', 'zope.exceptions',
-                'zope.i18n', 'zope.i18n.interfaces', 
-                'zope.i18nmessageid', 'zope.interface',
-                'zope.interface.common',
-                'zope.modulealias', 'zope.proxy', 'zope.schema',
-                'zope.testing',],
-      )
+packages = ['ccpublisher',
+            'p6', 'p6.app', 
+            'p6.configure', 'p6.zcmlsupport',
+            'p6.metadata', 
+            'p6.storage','p6.storage.providers',
+            'p6.ui', 'p6.ui.pages', 'p6.ui.windows',
+            'pyarchive', 'ccrdf', 'cctagutils', 'tagger', 'ccwsclient',
+            'ccwx','eyeD3',
+            'rdflib',
+            'rdflib.backends',
+            'rdflib.syntax',
+            'rdflib.syntax.serializers',
+            'rdflib.syntax.parsers',
+            'rdflib.model',
+            'zope',
+            'zope.component', 'zope.component.bbb',
+            'zope.component.bbb.tests',
+            'zope.configuration', 'zope.deprecation',
+            'zope.event', 'zope.exceptions',
+            'zope.i18n', 'zope.i18n.interfaces', 
+            'zope.i18nmessageid', 'zope.interface',
+            'zope.interface.common',
+            'zope.modulealias', 'zope.proxy', 'zope.schema',
+            'zope.testing',]
+                    
+if __name__ == '__main__':
+    
+    setup(name='ccPublisher',
+          version='1.9',
+          description = "Desktop tools for licensing works and uploading to the "
+                        "Internet Archive for hosting and cataloging.",
+          long_description="",
+          url='http://creativecommons.org',
+          author='Nathan R. Yergler',
+          author_email='nathan@creativecommons.org',
+          classifiers= ['Development Status :: 5 - Production/Stable',
+                        'Environment :: MacOS X :: Cocoa',
+                        'Environment :: Win32 (MS Windows)',
+                        'Environment :: X11 Applications :: GTK',
+                        'Intended Audience :: End Users/Desktop',
+                        'License :: OSI Approved :: GNU General Public License (GPL)',
+                        'Natural Language :: English',
+                        'Operating System :: OS Independent',
+                        'Programming Language :: Python',
+                        'Topic :: Multimedia',
+                        'Topic :: System :: Archiving',
+        ],
+          py_modules=['appsetup','setup' ],
+          scripts=['main.py'],
+          windows=['main.py'],
+          ext_modules=extensions,
+          data_files=[('resources', 
+                      ['resources/LICENSE.txt', 'resources/wizard.xrc'])] + 
+                      findZcml(),
+          packages=packages,
+          options={'py2exe':{'packages':packages,
+                             'includes':['dbhash', 'encodings',]
+                             }
+                   }
+          )
