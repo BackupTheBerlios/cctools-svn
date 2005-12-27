e185ee172dc5d99d74e167d5440fba96500a8c2a
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2005-12-27 15:54:10 +0000

    Added support for building a linux distribution via distutils using the distutils extension from Straw.  Added .desktop file and setup configuration file for storing common options and dependency information.

--- publisher/trunk/setup.py
+++ publisher/trunk/setup.py
@@ -1,9 +1,18 @@
 """
-ccTag build script
-Builds platform appropriate packages for distribution.  Takes standard
-distutils commands on non-Mac OS X platforms.  Builds a .app on Mac OS X.
+ccPublisher Build Script
+Builds platform appropriate packages for distribution.
 
-Nathan R. Yergler
+ Windows:
+ $ python appsetup.py py2exe
+
+ Mac OS X:
+ $ python appsetup.py py2app
+
+ Linux:
+ $ python appsetup.py bdist_rpm
+
+copyright 2004-2005, Nathan R. Yergler
+licensed to the public under the GNU GPL version 2.
 
 $Id$
 """
@@ -13,81 +22,30 @@ import os
 import platform
 import fnmatch
 
+from deploy.setupsupport import *
+from deploy.setupcfg import *
+
 PLATFORM = platform.system().lower()
 
+# import platform-specific distutils functionality
 if PLATFORM == 'windows':
     import py2exe
 elif PLATFORM == 'darwin':
     import py2app
-
-def findZcml():
-    result = []
-    
-    basePath = os.getcwd()
-    
-    for (dirpath, dirnames, filenames) in os.walk(basePath):
-        relpath = dirpath[len(basePath)+1:]
-        zcml = fnmatch.filter(filenames, "*.zcml")
-        
-        if len(zcml) > 0:
-            result.append((relpath, [os.path.join(relpath, n) for n in zcml]))
-
-    return result
-
-extensions = [Extension('zope.proxy._zope_proxy_proxy',
-                        ['zope/proxy/_zope_proxy_proxy.c'],
-                        include_dirs=[os.path.join('.','zope','proxy')],
-                        )
-              ]
-
-packages = ['ccpublisher',
-            'p6', 'p6.app', 
-            'p6.configure', 'p6.zcmlsupport',
-            'p6.metadata', 
-            'p6.storage','p6.storage.providers',
-            'p6.ui', 'p6.ui.pages', 'p6.ui.windows',
-            'pyarchive', 'ccrdf', 'cctagutils', 'tagger', 'ccwsclient',
-            'ccwx','eyeD3',
-            'rdflib',
-            'rdflib.backends',
-            'rdflib.syntax',
-            'rdflib.syntax.serializers',
-            'rdflib.syntax.parsers',
-            'rdflib.model',
-            'zope',
-            'zope.component', 'zope.component.bbb',
-            'zope.component.bbb.tests',
-            'zope.configuration', 'zope.deprecation',
-            'zope.event', 'zope.exceptions',
-            'zope.i18n', 'zope.i18n.interfaces', 
-            'zope.i18nmessageid', 'zope.interface',
-            'zope.interface.common',
-            'zope.modulealias', 'zope.proxy', 'zope.schema',
-            'zope.testing',]
+else:
+    from deploy.linux.straw_distutils import setup
                     
 if __name__ == '__main__':
     
     setup(name='ccPublisher',
           version='1.9',
-          description = "Desktop tools for licensing works and uploading to the "
-                        "Internet Archive for hosting and cataloging.",
-          long_description="",
+          description = desc,
+          long_description= long_desc,
           url='http://creativecommons.org',
           author='Nathan R. Yergler',
-          author_email='nathan@creativecommons.org',
-          classifiers= ['Development Status :: 5 - Production/Stable',
-                        'Environment :: MacOS X :: Cocoa',
-                        'Environment :: Win32 (MS Windows)',
-                        'Environment :: X11 Applications :: GTK',
-                        'Intended Audience :: End Users/Desktop',
-                        'License :: OSI Approved :: GNU General Public License (GPL)',
-                        'Natural Language :: English',
-                        'Operating System :: OS Independent',
-                        'Programming Language :: Python',
-                        'Topic :: Multimedia',
-                        'Topic :: System :: Archiving',
-        ],
-          py_modules=['appsetup','setup' ],
+          author_email='software@creativecommons.org',
+          classifiers= classifiers,
+          py_modules=['setup' ],
           scripts=['main.py'],
           windows=['main.py'],
           app=['main.py'],
@@ -96,11 +54,13 @@ if __name__ == '__main__':
                       ['resources/LICENSE.txt', 'resources/wizard.xrc'])] + 
                       findZcml(),
           packages=packages,
+          package_data=packageData(packages),
           options={'py2exe':{'packages':packages,
                              'includes':['dbhash', 'encodings',]
                              },
                    'py2app':{'packages':packages,
                              'includes':['dbhash', 'encodings',]
                              },
-                   }
+                   },
+          desktop_file=['deploy/linux/ccpublisher.desktop.in'],
           )
