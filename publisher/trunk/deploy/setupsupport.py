266913ac82b553cbb4a5d5e58da1db91ec59646e
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2005-12-27 15:45:06 +0000

    Move setup.py definitions into separate modules for clarity.

---
+++ publisher/trunk/deploy/setupsupport.py
@@ -0,0 +1,42 @@
+import os
+import fnmatch
+
+def __baseDir():
+    return ( os.path.dirname(__file__) or os.getcwd() )
+
+def findZcml():
+    """Walk the working directory for ZCML files which will be installed
+    alongside the byte-code files."""
+
+    IGNORE_DIRS = ('dist', 'build')
+    
+    result = []
+
+    # get the base path
+    basePath = __baseDir()
+
+    # walk the directory tree, ignoring distutils folders
+    for (dirpath, dirnames, filenames) in os.walk(basePath, topdown=True):
+
+        # prune out any distutils folders in subpaths
+        for ign in IGNORE_DIRS:
+            if ign in dirnames: dirnames.remove(ign)
+
+        # strip out the common portion of the path
+        relpath = dirpath[len(basePath)+1:]
+
+        # filter out only ZCML files
+        zcml = fnmatch.filter(filenames, "*.zcml")
+        
+        if len(zcml) > 0:
+            result.append((relpath, [os.path.join(relpath, n) for n in zcml]))
+
+    return result
+
+def packageData(pkgList):
+    result = {}
+
+    for package in pkgList:
+        result[package] = ['*.zcml']
+
+    return result
