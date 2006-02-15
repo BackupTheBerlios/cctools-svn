208bf4882d7a416e64693f45e2b9fc30db3e39b7
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2006-02-15 15:48:53 +0000

    Braindead typo.

--- libfeedback/trunk/libfeedback/comm.py
+++ libfeedback/trunk/libfeedback/comm.py
@@ -5,7 +5,7 @@ def sendReport(postUrl, fields):
     """Posts a crash or bug report to a specified URL and returns the response."""
     
     try:
-        f = urllib2.urlopen(postUrl, data=urllib.urlencode(field))
+        f = urllib2.urlopen(postUrl, data=urllib.urlencode(fields))
         bugUrl = f.read().strip()
         
         return bugUrl
