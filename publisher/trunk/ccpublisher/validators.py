9fe0be462bd94b51e52ebce07c064274f110ca4c
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2006-03-15 13:50:09 +0000

    Added description validator; closes issue 45.

--- publisher/trunk/ccpublisher/validators.py
+++ publisher/trunk/ccpublisher/validators.py
@@ -7,3 +7,10 @@ def validateTitle(title):
     else:
         return None
     
+def validateDescription(description):
+    """The Internet Archive requires that a description of 5 words is
+    provided; we just split on whitespace to determine words."""
+    
+    if not(description) or len(description.split()) < 5:
+        return "You must supply a description of at least 5 words."
+
