e6abcac76b0f90b35025311c6491161e94211e77
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2006-04-17 13:21:35 +0000

    Added subscription adapter for detecting the work format based on file mime-type; fix for Issue 1.

--- publisher/trunk/ccpublisher/adapters.py
+++ publisher/trunk/ccpublisher/adapters.py
@@ -1,9 +1,50 @@
+import wx
 import zope.interface
+import zope.component
 
 import p6.api
+import p6.storage.interfaces
+import p6.metadata.events
+
 import cctagutils.metadata
 import ccpublisher.interfaces
 
+MIME_MANAGER = wx.MimeTypesManager()
+
+def workFormatListener(event):
+    """Subscriber for IItemSelected events; attempts to determine the overall
+    work format from the file extension or mime-type.  If possible, publishes
+    an UpdateMetadata event."""
+
+    # check if this is a file item (the only type we know how to handle)
+    if p6.storage.interfaces.IFileItem in \
+           zope.interface.providedBy(event.item):
+
+        file_info = MIME_MANAGER.GetFileTypeFromExtension(
+            event.item.filename.split(".")[-1])
+        mime_type = file_info.GetMimeType()
+        
+        # check if this falls into one of our types
+        
+        if ("text" in mime_type):
+            type_value = "Text"
+        elif ("audio" in mime_type):
+            type_value = "Audio"
+        elif ("video" in mime_type):
+            type_value = "Video"
+        elif ("image" in mime_type):
+            type_value = "Image"
+        else:
+            # fall back to "Other" as the default if we can't figure it out
+            type_value = "Other"
+        
+        updateEvent = p6.metadata.events.UpdateMetadataEvent(
+            event.item,
+            "http://purl.org/dc/elements/1.1/type",
+            type_value)
+        
+        zope.component.handle(updateEvent)
+    
 class CcTagUtilsEmbeddable(object):
     zope.interface.implements(ccpublisher.interfaces.IEmbeddable)
 
