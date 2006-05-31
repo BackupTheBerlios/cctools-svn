5fbad25f5d7957e23aa43db3cd3187a4fd860e4e
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2006-05-31 16:46:57 +0000

    Additional convenience functions to wrap the wxPython i18n machinery.

--- publisher/trunk/p6/i18n/__init__.py
+++ publisher/trunk/p6/i18n/__init__.py
@@ -1,7 +1,32 @@
-
+import os
 import wx
 
 I18N_FILE = None
+LOCALE = None
+
+def initialize(root_dir):
+    """Initialize the wxPython internationalization machinery."""
+    global LOCALE
+    
+    LOCALE = wx.Locale(wx.LANGUAGE_DEFAULT, wx.LOCALE_LOAD_DEFAULT)
+    LOCALE.AddCatalogLookupPathPrefix(os.path.join(root_dir, 'locale'))
+
+    # load the framework catalog
+    loadCatalog('p6')
+
+def loadCatalog(catalog_name):
+    """Add a catalog to the lookup system."""
+
+    global LOCALE
+
+    LOCALE.AddCatalog(catalog_name)
+    
+def getLocale():
+    """Return the application locale."""
+
+    global LOCALE
+
+    return LOCALE.GetCanonicalName()
 
 def dump_ (str_arg):
     """Dump strings for translation to a text file."""
@@ -18,7 +43,7 @@ def dump_ (str_arg):
 
 
 # Uncomment the following line for development mode -- extract strings
-_ = dump_
+#_ = dump_
 
 # Uncomment the following line for production -- translate strings
-# _ = wx.GetTranslation
+_ = wx.GetTranslation
