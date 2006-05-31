2f45eef7f300aac8c57d663ca8d119395b8bf590
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2006-05-31 16:45:43 +0000

    Pass locale information to web service requests; fall back to english if we can't support the user's locale.

--- publisher/trunk/ccwsclient/rest.py
+++ publisher/trunk/ccwsclient/rest.py
@@ -4,16 +4,44 @@ import elementtree.ElementTree as etree
 class CcRest:
     """Wrapper class to decompose REST XML responses into Python objects."""
     
-    def __init__(self, root):
+    def __init__(self, root, lang='en'):
         self.root = root
 
         self.__lc_doc = None
 
-    def license_classes(self, lang='en'):
+        self.set_default_lang(lang)
+
+    def set_default_lang(self, lang):
+        """Set the default language for web service requests."""
+
+        # retrieve the list of supported locales
+        locale_url = '%s/locales' % self.root
+        locale_doc = etree.fromstring(urllib2.urlopen(locale_url).read())
+
+        # extract the list of support locales
+        supported_locales = [n.attrib['id'] for n in
+                             locale_doc.findall('locale')]
+
+        if lang not in supported_locales:
+            if lang.split('_')[0] in supported_locales:
+                # try the generic version of the language ("un-nationalized")
+                lang = lang.split('_')[0]
+                
+            else:
+                # fall back to English
+                lang = 'en'
+            
+        self.__default_lang = lang
+
+    def license_classes(self, lang=None):
         """Returns a dictionary whose keys are license IDs, with the
         license label as the value."""
 
-        lc_url = '%s/%s' % (self.root, 'classes')
+        # use the default language if none is specified
+        if lang is None:
+            lang = self.__default_lang
+            
+        lc_url = '%s/classes?locale=%s' % (self.root, lang)
 
         # retrieve the licenses document and store it
         self.__lc_doc = urllib2.urlopen(lc_url).read()
@@ -29,10 +57,14 @@ class CcRest:
             
         return lc
         
-    def fields(self, license, lang='en'):
+    def fields(self, license, lang=None):
         """Retrieves details for a particular license."""
 
-        l_url = '%s/license/%s' % (self.root, license)
+        # use the default language if none is specified
+        if lang is None:
+            lang = self.__default_lang
+            
+        l_url = '%s/license/%s?locale=%s' % (self.root, license, lang)
 
         # retrieve the license source document
         self.__l_doc = urllib2.urlopen(l_url).read()
@@ -66,13 +98,19 @@ class CcRest:
         self._cur_license['__keys__'] = keys
         return self._cur_license
 
-    def issue(self, license, answers, workinfo={}, lang='en'):
+    def issue(self, license, answers, workinfo={}, lang=None):
+
+        # use the default language if none is specified
+        if lang is None:
+            lang = self.__default_lang
+            
         l_url = '%s/license/%s/issue' % (self.root, license)
 
         # construct the answers.xml document from the answers dictionary
         answer_xml = """
         <answers>
-          <license-%s>""" % license
+          <locale>%s</locale>
+          <license-%s>""" % (lang, license)
 
         for key in answers:
             answer_xml = """%s
