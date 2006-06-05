a0932cc56f90861344af6317c7b69df89b1b37f9
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2006-06-05 14:59:40 +0000

    Recompile .po files on startup if they are newer than .mo files.

--- publisher/trunk/p6/i18n/catalog.py
+++ publisher/trunk/p6/i18n/catalog.py
@@ -2,6 +2,7 @@ import os
 import wx
 
 LOCALE = None
+PREFIXES = []
 
 def initialize(root_dir, locale):
     """Initialize the wxPython internationalization machinery; if locale is 
@@ -28,14 +29,62 @@ def initialize(root_dir, locale):
     loadCatalog('p6')
 
 def addCatalogPath(pathname):
-    global LOCALE
+    global LOCALE, PREFIXES
 
+    PREFIXES.append(pathname)
     LOCALE.AddCatalogLookupPathPrefix(pathname)
 
+def __findCatalog(paths, catalog, lang):
+    """Search for a catalog in paths, where paths is a sequence of possible
+    locations.  Searches <n>/<lang>, then <n> for <catalog>.[m|p]o for each
+    <n> in <paths>.  Returns the absolute path of the catalog.  If both
+    .mo and .po are found, returns the most recently updated one.  If the
+    catalog is not found, returns None."""
+    
+    for path in paths:
+        mo_file = os.path.abspath(os.path.join(path, lang, catalog + ".mo"))
+        po_file = os.path.abspath(os.path.join(path, lang, catalog + ".po"))
+        
+        po_exists = os.path.exists(po_file)
+        mo_exists = os.path.exists(mo_file)
+
+        if mo_exists and not(po_exists):
+            return mo_file
+        elif po_exists and not(mo_exists):
+            return po_file
+        elif po_exists and mo_exists:
+            # both exist; check which is newer
+            if os.path.getmtime(po_file) > os.path.getmtime(mo_file):
+                return po_file
+            else:
+                return mo_file
+
+    # catalog not found
+    return None
+            
+def compileCatalog(catalog_path):
+    """Compile the .po file specified by <catalog_path> to a binary gettext
+    catalog.  The resulting binary catalog is placed in the same directory
+    as the .po file."""
+
+    import msgfmt
+
+    # compile the catalog using msgfmt
+    return msgfmt.make(catalog_path,
+                       os.path.splitext(catalog_path)[0] + ".mo")
+    
 def loadCatalog(catalog_name):
     """Add a catalog to the lookup system."""
 
-    global LOCALE
+    global LOCALE, PREFIXES
+
+    # check if this catalog needs to be recompiled
+    catalog_path = __findCatalog(PREFIXES, catalog_name, getLocale())
+
+    if catalog_path is not None:
+        if os.path.splitext(catalog_path)[1] == '.po':
+            # recompile the catalog
+            compileCatalog(catalog_path)
 
     LOCALE.AddCatalog(catalog_name)
     
