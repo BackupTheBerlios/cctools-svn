ba8ed0f072ce7b742dfcb149c2576fe9ead9a524
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2006-06-07 13:08:17 +0000

    Fix for Issue 340.

--- publisher/branches/ccpublisher-2.0.x/ccpublisher/ia.py
+++ publisher/branches/ccpublisher-2.0.x/ccpublisher/ia.py
@@ -220,7 +220,7 @@ class ArchiveStorage(p6.metadata.base.BasicMetadataStorage,
            archive_id,
            self.archive_collection,
            self.submission_type,
-           api.findField('title')
+           api.findField('http://purl.org/dc/elements/1.1/title')
            )
 
        # retrieve all metadata fields for the work (the root item)
