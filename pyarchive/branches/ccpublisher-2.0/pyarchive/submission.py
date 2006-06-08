278cb1b062c3523dd92fd09b0614cf668763a0b6
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2006-06-08 12:43:28 +0000

    Incrementing version number to 2.0.4.2 for pyarchive branch.

--- pyarchive/branches/ccpublisher-2.0/pyarchive/submission.py
+++ pyarchive/branches/ccpublisher-2.0/pyarchive/submission.py
@@ -105,7 +105,7 @@ class ArchiveItem:
         <collection>%s</collection>
         <mediatype>%s</mediatype>
         <resource>%s</resource>
-        <upload_application appid="ccpublisher" version="2.0.4.1" />
+        <upload_application appid="ccpublisher" version="2.0.4.2" />
         """ % (xe(self.title),
                self.collection,
                self.mediatype,
@@ -178,7 +178,7 @@ class ArchiveItem:
         new_url = "/create.php"
         headers = {"Content-type": "application/x-www-form-urlencoded",
                    "Accept": "text/plain",
-                   "User-Agent": "ccPublisher 2.0.4.1"}
+                   "User-Agent": "ccPublisher 2.0.4.2"}
         
         params = urllib.urlencode({'xml':1,
                                    'user':username,
