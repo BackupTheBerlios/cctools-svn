b7b868885090f6f56eb573031110094d0b042978
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2006-06-11 01:50:12 +0000

    Properly inspect an URLError exception raised while checking identifier availability (Issue 385).

--- pyarchive/trunk/pyarchive/identifier.py
+++ pyarchive/trunk/pyarchive/identifier.py
@@ -48,14 +48,17 @@ def available(identifier):
                "identifier=%s" % identifier
 
     # make request
-    print checkurl
     try:
         response = urllib2.urlopen(checkurl)
-    except urllib2.URLError, (errno, strerror):
+    except urllib2.URLError, e:
+        errno = getattr(e, 'errno', None)
         if errno == 10054:
             # connection reset by peer
             # try again
             response = urllib2.urlopen(checkurl)
+        else:
+            # assume not available
+            return False
             
     response_dom = xml.dom.minidom.parse(response)
             
