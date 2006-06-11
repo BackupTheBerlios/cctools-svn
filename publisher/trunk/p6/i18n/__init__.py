c80ecdc0ee2a2c4c35a6216b0979ba210abf3549
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2006-06-11 21:41:32 +0000

    Check for the existence of sys.setdefaultencoding when we import p6.i18n; if it still exists, we're probably running in a frozen distribution (py2exe, etc), so set the encoding to utf-8.

--- publisher/trunk/p6/i18n/__init__.py
+++ publisher/trunk/p6/i18n/__init__.py
@@ -1,10 +1,15 @@
 """p6.i18n
 
 """
+import sys
 
+# check if sys.setdefaultencoding still exists; if it does, we're probably running frozen
+if getattr(sys, 'setdefaultencoding', False):
+    sys.setdefaultencoding('utf-8')
+    del sys.setdefaultencoding
+    
 from catalog import *
 from translate import *
-
 
 # Uncomment the following line for development mode -- extract strings
 #_ = dump_
