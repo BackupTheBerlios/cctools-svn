93208db486ef220b4151f550fd69f031c86b5d04
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2006-06-14 19:25:26 +0000

    Initialize the .uri attribute (Issue 422).

--- publisher/branches/ccpublisher-2.0.x/ccpublisher/ia.py
+++ publisher/branches/ccpublisher-2.0.x/ccpublisher/ia.py
@@ -161,6 +161,9 @@ class ArchiveStorage(p6.metadata.base.BasicMetadataStorage,
     def __init__(self):
         p6.metadata.base.BasicMetadataStorage.__init__(self)
 
+        # initialize the URI
+        self.uri = ''
+        
         # register handlers for extension points --
         # this allows us to extend the user interface in a unified way
         # 
