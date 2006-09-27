be3fc022e755cc40028c15343e667efa14d655b0
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2006-09-27 14:58:01 +0000

    Landing new wizard framework: core window class.

--- publisher/trunk/p6/ui/wizard.py
+++ publisher/trunk/p6/ui/wizard.py
@@ -2,51 +2,125 @@
 Customized wizard framework -- top level window classes.
 """
 
+import sys
+
 import wx
+import wx.wizard
 import wx.xrc
 
 import zope.interface
 import zope.configuration.xmlconfig
-import ccwx.xrcwiz
 
 import p6
 import p6.api
+import p6.ui.xrc
 import interfaces
+import pagecollection
 
+def call_sink(*args, **kwargs):
+    pass
 
-class WizFrame(ccwx.xrcwiz.XrcWiz):
-    """Top level wizard window."""
+def makeHeader(wizPg, title):
+    sizer = wx.BoxSizer(wx.VERTICAL)
+    wizPg.SetSizer(sizer)
+    title = wx.StaticText(wizPg, -1, title)
+    title.SetFont(wx.Font(18, wx.SWISS, wx.NORMAL, wx.BOLD))
+    sizer.Add(title, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
+    sizer.Add(wx.StaticLine(wizPg, -1), 0, wx.EXPAND|wx.ALL, 5)
+    return sizer
+
+class P6WizardPage(wx.wizard.PyWizardPage):
+    def __init__(self, parent, title):
+        wx.wizard.PyWizardPage.__init__(self, parent)
+        self.sizer = makeHeader(self, title)
+
+    @property
+    def __list(self):
+        # XXX lookup utility?
+        return p6.api.getApp().pages
+
+    pagelist = __list
     
-    def __init__(self, xrcid, app):
-        """
-        @param xrcid: The identifier of the XRC object in the application's
-           XRC file which defines our chrome.
-        @type xrcid: string
+    def GetNext(self):
 
-        @param app: The application object.
-        @type app: L{wx.App}
-        """
+        print "in GetNext "
+
+        if self.__list.is_last():
+            return None
+        else:
+            return self.__list.next()(self.GetParent())
+    
+    def GetPrev(self):
+
+        print "in getprev"
         
-        ccwx.xrcwiz.XrcWiz.__init__(self, app, app.xrcfile, xrcid)
+        if self.__list.is_first():
+            return None
+        else:
+            return self.__list.previous()(self.GetParent())
+      
+    def validate(self, event):
+        
+        return True
 
-        self.app = app
-        self.setPages([n(self) for n in p6.api.getApp().pages])
+    def onChanging(self, event):
+        pass
 
-    def setPages(self, pages):
-        """Updates the list of pages this wizard manages and resets the view
-        to the first page.
+    def onChanged(self, event):
+        pass
 
-        @param pages: Sequence of pages implementing
-           L{p6.ui.interfaces.IWizardPage}.
-        @type pages: sequence
-        """
+class XRCWizardPage(P6WizardPage):
+    def __init__(self, parent, title, xrcfile, xrcid ):
+        P6WizardPage.__init__(self, parent, title)
 
-        self.setPageCollection(ccwx.xrcwiz.PageCollection(pages))
-        self.addCurrent(None)
+        # load the user interface
+        self.__loadXrcUi(xrcfile, xrcid)
 
-        self.pages.current().Show()
+    def __loadXrcUi(self, xrcfile, xrcid):
 
-        self.SetAutoLayout(True)
-        self.Show()
-        self.Layout()
-    
+        self.sizer.Add(p6.ui.xrc.load(xrcfile).LoadPanel(self, xrcid),
+                       flag = wx.EXPAND)
+
+class P6Wizard(wx.wizard.Wizard):
+    def __init__(self, parent, id, title, pagelist=None):
+        wx.wizard.Wizard.__init__(self, parent, id, title)
+
+        # store a reference to the page collection
+        self.setModel(pagelist)
+        print len(self.getModel())
+        
+        # attach event handlers
+        self.Bind(wx.wizard.EVT_WIZARD_PAGE_CHANGED, self.OnWizardPageChanged)
+        self.Bind(wx.wizard.EVT_WIZARD_PAGE_CHANGING, self.OnWizardPageChanging)
+        self.Bind(wx.wizard.EVT_WIZARD_CANCEL, self.OnWizardCancel)
+
+    def start(self):
+        print self.__pages.current()(self)
+        self.RunWizard(self.__pages[0](self))
+        
+    def setModel(self, pagelist):
+
+        if pagelist is None:
+            pagelist = p6.api.getApp().pages
+            
+        if isinstance(pagelist, list):
+            self.__pages = pagecollection.PageCollection(pagelist)
+        else:
+            self.__pages = pagelist
+
+    def getModel(self):
+        """Return the PageCollection model used for this wizard."""
+        return self.__pages
+
+    def OnWizardPageChanged(self, event):
+        
+        getattr(event.GetPage(), 'onChanged', call_sink)(event)
+
+    def OnWizardPageChanging(self, event):
+        getattr(event.GetPage(), 'validate', call_sink)(event)
+
+        if event.IsAllowed():
+            getattr(event.GetPage(), 'onChanging', call_sink)(event)
+
+    def OnWizardCancel(self, event):
+        sys.exit()
