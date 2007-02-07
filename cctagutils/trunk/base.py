42352d8bd4dde3dc4b63e8bf4b30949918655259
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2007-02-07 22:51:57 +0000

    Added methods to the base class for handling verification.

--- cctagutils/trunk/base.py
+++ cctagutils/trunk/base.py
@@ -17,6 +17,11 @@ class BaseMetadata(object):
 
     def setClaim(self, claim):
         raise NotImplementedError()
+
+    def getLicense(self):
+        """Return the license URL."""
+
+        raise NotImplementedError()
     
     def embed(self, license, verification, year, holder):
         """Embed a license claim in the audio file."""
@@ -26,6 +31,12 @@ class BaseMetadata(object):
         """Returns true if the user has permission to change the metadata."""
         raise NotImplementedError()
 
+    def verify(self):
+        """Attempt to verify the embedded claim.  Return one of the VERIFY_*
+        constants defined in cctagutil."""
+
+        raise NotImplementedError()
+    
     def properties(self):
         """Return a sequence of property keys for metadata on this object."""
 
