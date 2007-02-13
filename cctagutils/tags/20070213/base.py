52122756251899d6e47cbe9d930788e40c5b9931
nyergler <nyergler@4f28f1e7-3f06-0410-a4b1-d3c5d52102a4>
2007-02-13 12:47:27 +0000

    Tagging current state.

---
+++ cctagutils/tags/20070213/base.py
@@ -0,0 +1,49 @@
+
+class BaseMetadata(object):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def getTitle(self):
+        raise NotImplementedError()
+
+    def getArtist(self):
+        raise NotImplementedError()
+
+    def getYear(self):
+        raise NotImplementedError()
+
+    def getClaim(self):
+        raise NotImplementedError()
+
+    def setClaim(self, claim):
+        raise NotImplementedError()
+
+    def getLicense(self):
+        """Return the license URL."""
+
+        raise NotImplementedError()
+    
+    def embed(self, license, verification, year, holder):
+        """Embed a license claim in the audio file."""
+        raise NotImplementedError()
+
+    def isWritable(self):
+        """Returns true if the user has permission to change the metadata."""
+        raise NotImplementedError()
+
+    def verify(self):
+        """Attempt to verify the embedded claim.  Return one of the VERIFY_*
+        constants defined in cctagutil."""
+
+        raise NotImplementedError()
+    
+    def properties(self):
+        """Return a sequence of property keys for metadata on this object."""
+
+        raise NotImplementedError()
+
+    def __getitem__(self, key):
+        """Return an additional metadata property for this object."""
+
+        raise KeyError("Unknown key %s" % key)
+        
